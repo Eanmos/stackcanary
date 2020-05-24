48f8fd3b334409c40fd70a18068e774ae6cba021
Nefedov Danil <eanmos@ya.ru>
2020-05-24 11:37:20 +0400

    Added Web Security Config.

--- src/main/java/com/sstu/StackCanary/configuration/MvcConfig.java
+++ src/main/java/com/sstu/StackCanary/configuration/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.sstu.StackCanary.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,10 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/q").setViewName("question");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
