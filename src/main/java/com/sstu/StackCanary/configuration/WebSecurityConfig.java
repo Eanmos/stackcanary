954c99eb89394320f461ca0d8afeb211abee1acb
Nefedov Danil <eanmos@ya.ru>
2020-05-25 19:10:31 +0400

    Added redirect to the index page on logout.

--- src/main/java/com/sstu/StackCanary/configuration/WebSecurityConfig.java
+++ src/main/java/com/sstu/StackCanary/configuration/WebSecurityConfig.java
@@ -27,6 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and()
                 .logout()
+                    .logoutSuccessUrl("/")
                     .permitAll();
     }
 
