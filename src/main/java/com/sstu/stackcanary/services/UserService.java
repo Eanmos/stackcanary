a16be8843b3b101e09e5b09d9bb435251727e29e
Nefedov Danil <eanmos@ya.ru>
2020-06-04 19:10:30 +0400

    Removed @Autowired on the UserService class.

--- src/main/java/com/sstu/stackcanary/services/UserService.java
+++ src/main/java/com/sstu/stackcanary/services/UserService.java
@@ -1,16 +1,16 @@
 package com.sstu.stackcanary.services;
 
 import com.sstu.stackcanary.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService {
-    @Autowired
-    private UserRepository userRepository;
+    final private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
