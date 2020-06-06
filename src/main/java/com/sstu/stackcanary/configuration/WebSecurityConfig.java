8eb173919711f4e9b346ffc52ec88b6fa4df021d
Nefedov Danil <eanmos@ya.ru>
2020-06-06 21:10:26 +0400

    Added 404 page.

--- src/main/java/com/sstu/stackcanary/configuration/WebSecurityConfig.java
+++ src/main/java/com/sstu/stackcanary/configuration/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/q", "/registration", "/static/**").permitAll()
+                    .antMatchers("/", "/404", "/error", "/q", "/registration", "/static/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
