43d06a7d59024cbff49fd1355a74d3f53eeb08cf
Nefedov Danil <eanmos@ya.ru>
2020-06-06 10:58:28 +0400

    Removed @Autowired in WebSecurityConfig.

--- src/main/java/com/sstu/stackcanary/configuration/WebSecurityConfig.java
+++ src/main/java/com/sstu/stackcanary/configuration/WebSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.sstu.stackcanary.configuration;
 
 import com.sstu.stackcanary.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,9 +11,9 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private UserService userService;
+    final private UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
